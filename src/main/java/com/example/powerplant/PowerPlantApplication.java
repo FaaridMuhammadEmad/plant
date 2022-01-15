94e921744454ed142a7ae8336a461cca1e290eec
Faarid <faarid@vinncorp.com>
2022-01-15 15:09:58 +0500

    merge

--- src/main/java/com/example/powerplant/PowerPlantApplication.java
+++ src/main/java/com/example/powerplant/PowerPlantApplication.java
@@ -1,25 +1,16 @@
 package com.example.powerplant;
 
-import com.example.powerplant.plant.model.Plant;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.core.Ordered;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
 
-import java.util.concurrent.Executors;
 
 
 @SpringBootApplication
